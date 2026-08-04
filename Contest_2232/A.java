0e2bf30e9c8dd2bdf4d53f9056a78b03bf4ecf03
dev09aj <anshika.24mei10131@vitbhopal.ac.in>
2026-08-04 18:09:55 +0530

    Create A.java

---
+++ Contest_2232/A.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] a = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(a);
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (a[i] != a[n - 1 - i]) {
+                    cnt++;
+                }
+            }
+
+            System.out.println(cnt / 2);
+        }
+    }
+}
