dd57cbf3f6d7a298d5d655c2b878ee5ce44e55f9
dev09aj <anshika.24mei10131@vitbhopal.ac.in>
2026-09-21 23:48:26 +0530

    Create A.java

---
+++ Contest_2226/A.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int ans = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (a[i] > 1) {
+                    ans += a[i];
+                }
+            }
+
+            if (a[n - 1] == 1) {
+                ans++;
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
